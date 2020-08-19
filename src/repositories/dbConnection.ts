45f01fd662acc597a1335163f74ed7f050d5e4e4
Ilya Kanishev <kanishev2002@gmail.com>
2020-08-19 23:42:55 +0300

    Moved .env logic to class EnvManager

--- src/repositories/dbConnection.ts
+++ src/repositories/dbConnection.ts
@@ -1,14 +1,6 @@
 import { Connection, createConnection } from 'typeorm';
+import { EnvManager } from '../types/EnvManager';
 
-export const getDbConnection = async (): Promise<Connection> => createConnection({
-  name: process.env.PG_CONNECTION_NAME || 'default',
-  type: 'postgres',
-  host: process.env.PG_HOST || '127.0.0.1',
-  port: Number(process.env.PG_PORT) || 5432,
-  username: process.env.PG_USERNAME || 'postgres',
-  password: process.env.PG_PASSWORD || '1q2w3e',
-  database: process.env.PG_DB_NAME || 'postgres',
-  schema: process.env.PG_SCHEMA || 'sky',
-  synchronize: false,
-  entities: [`${__dirname}/entities/*.js`],
-});
+export const getDbConnection = async (): Promise<Connection> => createConnection(
+  EnvManager.getDBConnectionSettings(),
+);
