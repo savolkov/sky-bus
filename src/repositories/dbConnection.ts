df32d96f3f891ee4e6323ce0dd392f031040c6a8
Ilya Kanishev <kanishev2002@gmail.com>
2020-08-19 22:41:59 +0300

    Fix: changed variable names and added .env file

--- src/repositories/dbConnection.ts
+++ src/repositories/dbConnection.ts
@@ -1,14 +1,14 @@
 import { Connection, createConnection } from 'typeorm';
 
 export const getDbConnection = async (): Promise<Connection> => createConnection({
-  name: process.env.CONNECTION_NAME || 'default',
+  name: process.env.PG_CONNECTION_NAME || 'default',
   type: 'postgres',
-  host: process.env.HOST || '127.0.0.1',
-  port: Number(process.env.PORT) || 5432,
-  username: process.env.USERNAME || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || '1q2w3e',
-  database: process.env.DATABASE || 'postgres',
-  schema: process.env.SCHEMA || 'sky',
+  host: process.env.PG_HOST || '127.0.0.1',
+  port: Number(process.env.PG_PORT) || 5432,
+  username: process.env.PG_USERNAME || 'postgres',
+  password: process.env.PG_PASSWORD || '1q2w3e',
+  database: process.env.PG_DB_NAME || 'postgres',
+  schema: process.env.PG_SCHEMA || 'sky',
   synchronize: false,
   entities: [`${__dirname}/entities/*.js`],
 });
