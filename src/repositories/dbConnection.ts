541be51e18a36e8ab2312d7190c851b0dd20aed1
Ilya Kanishev <kanishev2002@gmail.com>
2020-08-19 13:04:56 +0300

    Fix: moved DB connection settings to .env

--- src/repositories/dbConnection.ts
+++ src/repositories/dbConnection.ts
@@ -1,14 +1,14 @@
 import { Connection, createConnection } from 'typeorm';
 
 export const getDbConnection = async (): Promise<Connection> => createConnection({
-  name: 'default',
+  name: process.env.CONNECTION_NAME || 'default',
   type: 'postgres',
-  host: '127.0.0.1',
-  port: 5432,
-  username: 'postgres',
-  password: '1q2w3e',
-  database: 'postgres',
-  schema: 'sky',
+  host: process.env.HOST || '127.0.0.1',
+  port: Number(process.env.PORT) || 5432,
+  username: process.env.USERNAME || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || '1q2w3e',
+  database: process.env.DATABASE || 'postgres',
+  schema: process.env.SCHEMA || 'sky',
   synchronize: false,
   entities: [`${__dirname}/entities/*.js`],
 });
