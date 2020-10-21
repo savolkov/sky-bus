eae31f18c827e2ce15a31bf569b67c9bab4e3883
f883 <souprise@ya.ru>
2020-10-21 15:24:15 +0500

    Add: persone time calculation methods for sigur

---
+++ src/endpoints/index.ts
@@ -0,0 +1,3 @@
+// export * from './gatewayAccessDenied';
+// export * from './gatewayAccessRequired';
+// export * from './gatewayPassed';
--- src/handlers/sigur/gatewayAccessRequired.ts
+++ src/handlers/sigur/gatewayAccessRequired.ts
@@ -1,17 +1,55 @@
 import { HandlesMessage, BUS_SYMBOLS, Bus } from '@node-ts/bus-core';
 import { inject } from 'inversify';
 import { LOGGER_SYMBOLS, Logger } from '@node-ts/logger-core';
+import { Repository } from 'typeorm';
 import { SigurGatewayAccessRequired } from '../../messages/sigur';
+import { GatewayPassEvent } from '../../repositories/entities/GatewayPassEvent';
+import { REPOSITORIES } from '../../repositories/types';
 
 @HandlesMessage(SigurGatewayAccessRequired)
 export class SigurGatewayAccessRequiredHandler {
   constructor(
     @inject(BUS_SYMBOLS.Bus) private readonly bus: Bus,
     @inject(LOGGER_SYMBOLS.Logger) private readonly logger: Logger,
+    @inject(REPOSITORIES.GatewayPassEventRepository)
+    private readonly gatewayPassEventRepository: Repository<GatewayPassEvent>,
   ) {
   }
 
+  private async createGatewayPassEvent( // TODO: вынести функцию для доступа из других файлов
+    extenalId: number | null,
+    isPassed: boolean,
+    gatewayId: number,
+    isIngoingDirection: boolean,
+    personeId: string | null,
+    passCardUuid: string | null,
+    timestamp: number,
+  ): Promise<GatewayPassEvent> {
+    const { gatewayPassEventRepository } = this;
+    const draft = gatewayPassEventRepository.create();
+    draft.extenalId = extenalId;
+    draft.isPassed = isPassed;
+    draft.gatewayId = gatewayId;
+    draft.isIngoingDirection = isIngoingDirection;
+    draft.personeId = personeId;
+    draft.passCardId = passCardUuid;
+    draft.timestamp = timestamp;
+    return gatewayPassEventRepository.save(draft);
+  }
+
   async handle(sigurMessage: SigurGatewayAccessRequired): Promise<void> {
+    const { sigurEvent: event } = sigurMessage;
+
+    this.createGatewayPassEvent(
+      event.gatewayId,
+      event.isGatewayPassed,
+      event.gatewayId,
+      event.isIngoingDirection,
+      event.uuidPersone,
+      event.passCardId,
+      event.timestamp,
+    );
+
     this.logger.info(
       `SigurGatewayAccessRequired event received, device uuid: ${sigurMessage.sigurEvent.gatewayId}\n`,
       sigurMessage,
