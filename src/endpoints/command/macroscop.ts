2c72a3ccfc3f249085ec91f04e3bc6e0f803ccba
f883 <souprise@ya.ru>
2020-11-24 18:59:44 +0500

    Add: macroscop get cameras methods

--- src/endpoints/command/macroscop.ts
+++ src/endpoints/command/macroscop.ts
@@ -2,28 +2,51 @@ import * as express from 'express';
 import {
   interfaces,
   controller,
-  httpPost,
   request,
   response,
+  httpGet,
 } from 'inversify-express-utils';
 import { inject } from 'inversify';
 import { Bus, BUS_SYMBOLS } from '@node-ts/bus-core';
-import { MacroscopWarningRecieved } from '../../messages';
-import { MacroscopWarning } from '../../types/macroscopWarning';
+import { config } from 'dotenv';
+import { MacroscopApi } from '../../libs/api/macroscop';
+import { MacroscopAdapter } from '../../adapters/macroscop';
+
+config();
 
 @controller('/macroscop')
 export class MacroscopController implements interfaces.Controller {
+  private macroscopAdapter: any;
+
   constructor(
     @inject(BUS_SYMBOLS.Bus) private readonly bus: Bus,
   ) {
+    const macroscopApi = new MacroscopApi(
+      process.env.MACROSCOP_HOST || '',
+    );
+    this.macroscopAdapter = new MacroscopAdapter(macroscopApi);
   }
 
-  @httpPost('/')
-  private async create(@request() req: express.Request, @response() res: express.Response) {
-    const registeredMacroscopEvent: MacroscopWarning = { ...req.body, timestamp: new Date() };
-    this.bus.publish(new MacroscopWarningRecieved(registeredMacroscopEvent)).then(
-      () => res.sendStatus(204),
-    )
-      .catch(() => res.sendStatus(500));
+  @httpGet('/cameras')
+  private async list(@request() req: express.Request, @response() res: express.Response) {
+    try {
+      const cameras = await this.macroscopAdapter.getCameras();
+      res.status(200).send(cameras);
+    } catch (ex) {
+      console.log(ex);
+      res.sendStatus(500);
+    }
+  }
+
+  @httpGet('/cameras/:uuid')
+  private async info(@request() req: express.Request, @response() res: express.Response) {
+    const { uuid } = req.params;
+    try {
+      const camera = await this.macroscopAdapter.getCameraInfo(uuid);
+      res.status(200).send(camera);
+    } catch (ex) {
+      console.log(ex);
+      res.sendStatus(500);
+    }
   }
 }
---
+++ src/endpoints/event/old.macroscop.ts
@@ -0,0 +1,29 @@
+import * as express from 'express';
+import {
+  interfaces,
+  controller,
+  httpPost,
+  request,
+  response,
+} from 'inversify-express-utils';
+import { inject } from 'inversify';
+import { Bus, BUS_SYMBOLS } from '@node-ts/bus-core';
+import { MacroscopWarningRecieved } from '../../messages';
+import { MacroscopWarning } from '../../types/macroscopWarning';
+
+@controller('/macroscop')
+export class MacroscopController implements interfaces.Controller {
+  constructor(
+    @inject(BUS_SYMBOLS.Bus) private readonly bus: Bus,
+  ) {
+  }
+
+  @httpPost('/')
+  private async create(@request() req: express.Request, @response() res: express.Response) {
+    const registeredMacroscopEvent: MacroscopWarning = { ...req.body, timestamp: new Date() };
+    this.bus.publish(new MacroscopWarningRecieved(registeredMacroscopEvent)).then(
+      () => res.sendStatus(204),
+    )
+      .catch(() => res.sendStatus(500));
+  }
+}
